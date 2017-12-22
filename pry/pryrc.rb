fc27b5ff51be9b3b2e3da1ada923808cdbc303d0
Scott Pierce <scott.pierce@centro.net>
2017-12-22 03:59:09 -0600

    Improve pryrc

--- pry/pryrc.rb
+++ pry/pryrc.rb
@@ -1,2 +1,13 @@
-puts("Pry history: #{history_file = "~/.pry_history_#{File.basename(Dir.pwd)}"}")
-Pry.config.history.file = File.expand_path(history_file)
+local_pry_history = File.join(Dir.pwd, '.pry_history')
+if File.file? local_pry_history
+  puts("\e[33mUsing local Pry history:\e[0m #{local_pry_history}")
+  Pry.config.history.file = local_pry_history
+end
+
+# Setting max with to handle UUIDs
+begin
+  require 'table_print'
+  tp.set :max_width, 40
+rescue LoadError
+  puts 'table_print not available'
+end
