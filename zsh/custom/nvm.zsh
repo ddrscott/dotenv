e6636fe2382b163d6b540e64700df378539df003
Scott Pierce <ddrscott@gmail.com>
2021-07-23 09:19:43 -0500

    Fix nvm not loading

--- zsh/custom/nvm.zsh
+++ zsh/custom/nvm.zsh
@@ -1,7 +1,11 @@
-export NVM_DIR="$HOME/.nvm"
-export NVM_DEFAULT_VERSION_PATH=${NVM_DIR}/versions/node/v14.17.3/bin
-if [ -d $NVM_DEFAULT_VERSION_PATH ]; then
-  export PATH="${PATH}:${NVM_DEFAULT_VERSION_PATH}"
-elif [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
-  source "/usr/local/opt/nvm/nvm.sh"
-fi
+function nvm {
+  unset -f -m 'nvm'
+  export NVM_DIR="$HOME/.nvm"
+  export NVM_DEFAULT_VERSION_PATH=${NVM_DIR}/versions/node/v14.17.3/bin
+  if [ -d $NVM_DEFAULT_VERSION_PATH ]; then
+    export PATH="${PATH}:${NVM_DEFAULT_VERSION_PATH}"
+  elif [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
+    source "/usr/local/opt/nvm/nvm.sh"
+  fi
+  nvm $*
+}
