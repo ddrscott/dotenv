26fe8a65de53ab7aa1972230305e706ee882ccfa
Scott Pierce <ddrscott@gmail.com>
2023-12-04 11:42:03 -0600

    Try to speed up NVM loading
    https://www.ioannispoulakas.com/2020/02/22/how-to-speed-up-shell-load-while-using-nvm/

--- zsh/custom/nvm.zsh
+++ zsh/custom/nvm.zsh
@@ -1,4 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-if [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ]; then
-  source "$(brew --prefix)/opt/nvm/nvm.sh"
+if [[ -d "$HOME/.nvm/versions/node/" ]]; then
+  local latest_dir=$(ls -d1 "$HOME/.nvm/versions/node/"* | tail -1)
+  export PATH="$latest_dir/bin:$PATH"
+  export NVM_DIR=$HOME/.nvm
+  [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh" --no-use
 fi
